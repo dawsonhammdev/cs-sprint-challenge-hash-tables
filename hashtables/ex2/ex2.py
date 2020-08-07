e9df7c85c6e7f0bc969928833d56c944e0210031
dawsonhammdev <hammsdesigns@gmail.com>
2020-08-07 18:48:23 -0500

    I have 2 problems passing working on the third still

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -5,10 +5,27 @@ class Ticket:
         self.destination = destination
 
 
-def reconstruct_trip(tickets, length):
+def reconstruct_trip(self, tickets):
     """
     YOUR CODE HERE
     """
     # Your code here
+    d = {}
+    # for loop through the tickets
+    for i in tickets:
+        if self.source is None:
+            i += d[i]
+        if self.source is self.destination:
+            i += d[i]
+        if self.desitnation is self.source:
+            i += d[i]
+    
+    for j in d:
+        route.append(j)
+
+    # create route
+    route = []
+    #orgnize the tickets
+
 
     return route
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -5,15 +5,22 @@ def intersection(arrays):
     # Your code here
     # initialize a dict:
     d = {}
+    result = []
 
     # we need to go through the arrays and when a number shows up in both arrays we need to add it to the d and return the d
     for i in arrays:
-        for j in arrays:
+        for j in i:
             if j not in d:
                 d[j] = 1
             else:
                 d[j]+=1
-    result = []
+
+    for x in d:
+        if d[x] == len(arrays):
+            result.append(x)
+
+    # need to compare the values in the dictonary with the length of the arrays to see if there are dupicates and then return the results.
+    
 
     return result
 
